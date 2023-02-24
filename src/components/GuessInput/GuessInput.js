17bf29dfdbe6c447e85b7902a7b0b34637e5e9c1
Bart Sluis <b.sluis1993@gmail.com>
2023-02-24 12:14:22 +0100

    feat(Guess)
    Fix className errors
    Add NUM_OF_CHARS_ALLOWED
    Add Guess component

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {NUM_OF_CHARS_ALLOWED} from "../../constants"
 
 function GuessInput({ addGuess }) {
   const [guess, setGuess] = useState('')
@@ -15,10 +16,12 @@ function GuessInput({ addGuess }) {
     setGuess('');
   }
 
+  const pattern = `\\w{${NUM_OF_CHARS_ALLOWED},${NUM_OF_CHARS_ALLOWED}}`
+
   return (
-    <form class="guess-input-wrapper" onSubmit={handleSubmit}>
+    <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" value={guess} onChange={handleGuessChange} pattern="\w{5,5}" />
+      <input id="guess-input" type="text" value={guess} onChange={handleGuessChange} pattern={pattern} />
     </form>
   );
 }
--- src/components/GuessList/GuessList.js
+++ src/components/GuessList/GuessList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Guess from "../Guess/Guess";
+import { range } from "../../utils";
+import {NUM_OF_GUESSES_ALLOWED} from "../../constants"
 
 function GuessList({ guesses = [] }) {
   // using index as key, because changes can't be edited after submit.
 
-  return <div class="guess-results">
-    {guesses.map((guess, guessIdx) => <p class="guess" key="guessIdx">{guess}</p>)}
+  return <div className="guess-results">
+    {range(NUM_OF_GUESSES_ALLOWED).map((guessIdx) => <Guess key={`result-${guessIdx}`} value={guesses[guessIdx]} />)}
   </div>;
 }
 
