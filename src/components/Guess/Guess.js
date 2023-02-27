a9614e2b84b21cfc9b5db70c6b1d36b92bc96648
Bart Sluis <b.sluis1993@gmail.com>
2023-02-27 13:08:51 +0100

    feat(Guess) Add guess validation check

--- src/components/Guess/Guess.js
+++ src/components/Guess/Guess.js
@@ -2,11 +2,27 @@ import React from "react";
 import { range } from "../../utils";
 import { NUM_OF_CHARS_ALLOWED } from "../../constants";
 
-function Guess({ value = '' }) {
-const chars = Array.from(value);
-
+function Guess({ value = [] }) {
   return <p className="guess">
-    {range(NUM_OF_CHARS_ALLOWED).map((charIdx) => <span className="cell" key={`char-${charIdx}`}>{chars[charIdx] || ''}</span>)}
+    {range(NUM_OF_CHARS_ALLOWED).map((charIdx) => {
+      if (value && value.length > 0) {
+        return (<span 
+          className={`cell ${value[charIdx].status}`} 
+          key={`char-${charIdx}`}
+        >
+          {value[charIdx].letter}
+        </span>
+        )
+      }
+      else {
+        return (<span 
+          className={`cell`} 
+          key={`char-${charIdx}`}
+        ></span>
+        )
+      }
+    } 
+    )}
   </p>;
 }
 
