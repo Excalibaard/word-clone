ca6cc3dc3284b80d83e1a306b792fee0fc5810e8
Bart Sluis <b.sluis1993@gmail.com>
2023-02-27 17:20:22 +0100

    feat(GameBanner)
    Add GameStatus
    Add game status check helpers
    Disable Input when game ended
    Move resetGame to banner

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -1,27 +1,34 @@
 import React, { useState } from "react";
-import {NUM_OF_CHARS_ALLOWED} from "../../constants"
+import { NUM_OF_CHARS_ALLOWED } from "../../constants";
 
-function GuessInput({ addGuess }) {
-  const [guess, setGuess] = useState('')
+function GuessInput({ addGuess, disabled = false }) {
+  const [guess, setGuess] = useState("");
 
-  const handleGuessChange = event => {
+  const handleGuessChange = (event) => {
     let newGuess = event.target.value;
-    newGuess = newGuess.toUpperCase()
+    newGuess = newGuess.toUpperCase();
     setGuess(newGuess);
-  }
+  };
 
-  const handleSubmit = event => {
+  const handleSubmit = (event) => {
     event.preventDefault();
     addGuess(guess);
-    setGuess('');
-  }
+    setGuess("");
+  };
 
-  const pattern = `[a-zA-Z]{${NUM_OF_CHARS_ALLOWED},${NUM_OF_CHARS_ALLOWED}}`
+  const pattern = `[a-zA-Z]{${NUM_OF_CHARS_ALLOWED},${NUM_OF_CHARS_ALLOWED}}`;
 
   return (
     <form className="guess-input-wrapper" onSubmit={handleSubmit}>
       <label htmlFor="guess-input">Enter guess:</label>
-      <input id="guess-input" type="text" value={guess} onChange={handleGuessChange} pattern={pattern} />
+      <input
+        id="guess-input"
+        type="text"
+        value={guess}
+        onChange={handleGuessChange}
+        pattern={pattern}
+        disabled={disabled}
+      />
     </form>
   );
 }
